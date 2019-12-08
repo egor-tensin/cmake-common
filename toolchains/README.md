2bae67a4721aa39d2c24e89e00fa07e221654a90
Egor Tensin <Egor.Tensin@gmail.com>
2019-12-08 19:05:15 +0300

    add toolchains

---
+++ toolchains/README.md
@@ -0,0 +1,3 @@
+Use the toolchain files by passing something like
+`-DCMAKE_TOOLCHAIN_FILE=path/to/cmake-common/toolchains/toolchain.cmake` to
+`cmake`.
---
+++ toolchains/mingw-w64-x64.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMILER   x86_64-w64-mingw32-windres)
