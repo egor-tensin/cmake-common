db2b16324217d66338b383d6039263501b53be0e
Egor Tensin <Egor.Tensin@gmail.com>
2020-01-13 08:06:22 +0300

    cmake/examples: run clang-format

--- cmake/examples/boost/foo.cpp
+++ cmake/examples/boost/foo.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello from " << boost::filesystem::absolute(boost::filesystem::path{argv[0]}).string() << "!\n";
+    std::cout << "Hello from "
+              << boost::filesystem::absolute(boost::filesystem::path{argv[0]})
+                     .string()
+              << "!\n";
     return 0;
 }
